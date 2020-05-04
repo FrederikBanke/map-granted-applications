fa0374151ecb491f370b70cefa6f3c9d9c55cd72
Frederik Banke <frederiksbanke@gmail.com>
2020-05-04 17:44:57 +0200

    More information in project preview

--- frontend/src/components/Overlay/Overlay.js
+++ frontend/src/components/Overlay/Overlay.js
@@ -4,10 +4,11 @@ import PropTypes from 'prop-types'
 function Overlay(props) {
     const overlayStyle = {
         position: "fixed", /* Sit on top of the page content */
-        width: "50%", /* Full width (cover the whole page) */
-        height: "50%", /* Full height (cover the whole page) */
+        width: "fit-content", /* Full width (cover the whole page) */
+        height: "fit-content", /* Full height (cover the whole page) */
         top: "50px",
         left: "50px",
+        right: "50px",
         zIndex: "2", /* Specify a stack order in case you're using a different order for other elements */
     }
 
@@ -18,7 +19,7 @@ function Overlay(props) {
 
     return (
         <div style={overlayStyle}>
-            <button onClick={onClickClose}>Close</button>
+            <button onClick={onClickClose} style={{float: "left"}}>Close</button>
             {
                 React.Children.map(props.children, (child => (
                     child
--- frontend/src/components/ProjectView/ProjectView.js
+++ frontend/src/components/ProjectView/ProjectView.js
@@ -1,22 +1,52 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { callApi } from '../../util/api';
+import { getPrimaryColor, getTertiaryColor } from '../../util/colors';
+
+/**
+ * The complete Triforce, or one or more components of the Triforce.
+ * @typedef {Object} Project
+ * @property {string} id - 
+ * @property {?string} acronym - 
+ * @property {?string} title - 
+ * @property {?string} objective - 
+ * @property {?string} rcn - 
+ * @property {?string} status - 
+ * @property {?string} programme - 
+ * @property {?string} topics - 
+ * @property {?string} frameworkProgramme - 
+ * @property {?string} startDate - 
+ * @property {?string} endDate - 
+ * @property {?string} projectUrl - 
+ * @property {?string} ecMaxContribution - 
+ * @property {?string} call - 
+ * @property {?string} fundingScheme - 
+ * @property {?string} coordinator - 
+ * @property {?string} coordinatorCountry - 
+ * @property {?string} participants - 
+ * @property {?string} participantCountries - 
+ */
+
 
 /**
  * 
  * @param {Object} props 
- * @param {Object} props.project
- * @param {String} props.id
+ * @param {Project} props.project
  * @param {Function} props.onProjectChange
  * 
  */
 function ProjectView(props) {
-    const [project, setProject] = useState(null);
+    const [project, setProject] = useState(props.project);
+    const [objectiveStyle, setObjectiveStyle] = useState({ display: "block" });
 
 
-    const style = {
+    const containerStyle = {
         backgroundColor: "white",
         border: "solid"
+    }
+
+    const headerStyle = {
+        color: getTertiaryColor()
     }
 
     useEffect(() => {
@@ -33,14 +63,31 @@ function ProjectView(props) {
     }, [props.id, props.project]);
 
     const onSaveClick = () => {
+
         props.onProjectChange(project);
+    }
+
+    const onClickObjective = () => {
+        if (objectiveStyle.display === "none") {
+            setObjectiveStyle({
+                display: "block"
+            });
+        } else {
+            setObjectiveStyle({
+                display: "none"
+            });
+        }
     }
 
     return (
         project
-            ? (<div style={style}>
-                <p>Title: {project.title}</p>
-                <p>Objective: {project.objective}</p>
+            ? (<div style={containerStyle}>
+                <h1 style={headerStyle}>Title: {project.title}</h1>
+                <p>Acronym: {project.acronym}</p>
+                <p>Project dates: {project.startDate} - {project.endDate} </p>
+                <h2 onClick={onClickObjective} >Objective</h2>
+                <p style={objectiveStyle}>{project.objective}</p>
+                {project.projectUrl ? <p>Read more at: {project.projectUrl}</p> : null}
                 <button onClick={onSaveClick}>Save and set project as active</button>
             </div>)
             : null
