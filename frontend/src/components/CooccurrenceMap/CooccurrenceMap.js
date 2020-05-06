6024174f55b00e0ec5d7778e416a3644d30f9a24
Frederik Banke <frederiksbanke@gmail.com>
2020-05-06 12:08:04 +0200

    Added file for co-occurrence map

---
+++ frontend/src/components/CooccurrenceMap/CooccurrenceMap.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CooccurrenceMap = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+CooccurrenceMap.propTypes = {
+
+}
+
+export default CooccurrenceMap
