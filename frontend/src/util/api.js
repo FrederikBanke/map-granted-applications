88fb840aaf87d2417930a4b4fb36a57b93dadbc6
Frederik Banke <frederiksbanke@gmail.com>
2020-05-15 20:14:37 +0200

    Changes to api settings
    Co-authored-by: Brandeborg <Hallager2505@gmail.com>

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -13,7 +13,8 @@ export const callApi = (endpoint, method = 'GET', body = null) => {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
-            }
+            },
+            mode: "cors"
         })
             .then(res => res.json())
     }
