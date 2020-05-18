fb268d8681587f18bcf498177a39532bdedaa7b5
Frederik Banke <frederiksbanke@gmail.com>
2020-05-18 15:26:33 +0200

    Change api

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$c$$#=#a4532ubvf(n!@9v9$27xojbifwyg^safel1uk45gu7p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['banke-ccbf3238.localhost.run', 'localhost']
+ALLOWED_HOSTS = ['af043a70.ngrok.io', 'localhost']
 
 
 # Application definition
